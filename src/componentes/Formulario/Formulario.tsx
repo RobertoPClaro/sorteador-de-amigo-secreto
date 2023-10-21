65a0b7c3c097bfac57d65e463c0afb8f1d311e6a
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2023-10-21 18:44:21 -0300

    Refatorando stulo do Formulario

--- src/componentes/Formulario/Formulario.tsx
+++ src/componentes/Formulario/Formulario.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './style.module.css'
 
 export default function Formulario() {
   return (
     <form>
-        <input type='text' placeholder='Insira os nomes dos participantes'/>
-        <button disabled={true}>Adicionar</button>
+        <input className={styles.input} type='text' placeholder='Insira os nomes dos participantes'/>
+        <button className={styles.button} disabled={true}>Adicionar</button>
     </form>
   )
 }
