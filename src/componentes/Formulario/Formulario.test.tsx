038832502c79ce86322e88e43b4f08b44b1b622e
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2023-10-23 07:03:54 -0300

    Refatorando formulario

--- src/componentes/Formulario/Formulario.test.tsx
+++ src/componentes/Formulario/Formulario.test.tsx
@@ -1,25 +1,133 @@
-import { render, screen } from '@testing-library/react'
+import { act, fireEvent, render, screen } from '@testing-library/react'
 import React from 'react'
 import Formulario from './Formulario'
+import { RecoilRoot } from 'recoil'
 
-//Jest
+describe('o comportamento do Formulario.tsx', () => {
+    test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
+        // Arrange
+        render(
+            <RecoilRoot>
+                <Formulario />
+            </RecoilRoot>
+        )
 
-test('quando o input está vazio, novos participantes não podem ser adicionados', () =>{
-    // Arrange
-    render(<Formulario/>)
+        // Act 
+        // encontrar no DOM o input
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+
+        // encontrar o botão 
+        const botao = screen.getByRole('button')
+
+        //Assert 
+        // garatir que o input esteja no documento
+        expect(input).toBeInTheDocument()
+
+        // garatir que o botão esteja desabilitado
+        expect(botao).toBeDisabled()
+    })
+
+    test('qadicionar um participante caso exista um nome preenchido', () => {
+        // Arrange
+        render(
+            <RecoilRoot>
+                <Formulario />
+            </RecoilRoot>
+        )
+
+        // Act 
+        // encontrar no DOM o input
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+
+        // encontrar o botão 
+        const botao = screen.getByRole('button')
+
+        //Assert 
+        //inserir um valor no input
+        fireEvent.change(input, {
+            target: {
+                value: 'Ana Catarina'
+            }
+        })
+
+        //clicar no botão de submeter 
+        fireEvent.click(botao)
+
+        //garantir que o input esteja com o foca ativo
+        expect(input).toHaveFocus()
+
+        //garatir que o input não tenha um valor 
+        expect(input).toHaveValue('')
+    })
+
+    test('nomes duplicados não podem ser adicionados na lista', () => {
+        // Arrange
+        render(
+            <RecoilRoot>
+                <Formulario />
+            </RecoilRoot>
+        )
+
+        // Act 
+        // encontrar no DOM o input
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+
+        // encontrar o botão 
+        const botao = screen.getByRole('button')
+
+        //Assert 
+        //inserir um valor no input
+        fireEvent.change(input, {
+            target: {
+                value: 'Ana Catarina'
+            }
+        })
+
+
+
+        //clicar no botão de submeter 
+        fireEvent.click(botao)
+
+        fireEvent.change(input, {
+            target: {
+                value: 'Ana Catarina'
+            }
+        })
+        fireEvent.click(botao)
+
+        const menssagemDeErro = screen.getByRole('alert')
+
+        expect(menssagemDeErro.textContent).toBe('Nomes duplicados não são permitidos!')
+    })
+
+    test('a mensagem de erro deve sumir após os timers', () => {
+        jest.useFakeTimers()
+        render(
+            <RecoilRoot>
+                <Formulario />
+            </RecoilRoot>)
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        const botao = screen.getByRole('button')
+        fireEvent.change(input, {
+            target: {
+                value: 'Ana Catarina'
+            }
+        })
+        fireEvent.click(botao)
+        fireEvent.change(input, {
+            target: {
+                value: 'Ana Catarina'
+            }
+        })
+        fireEvent.click(botao)
+        let mensagemDeErro = screen.queryByRole('alert')
+        expect(mensagemDeErro).toBeInTheDocument()
     
-    // Act 
-    // encontrar no DOM o input
-    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-
-    // encontrar o botão 
-    const botao = screen.getByRole('button')
+        act(() => {
+            jest.runAllTimers()
+        });
     
-    //Assert 
-    // garatir que o input esteja no documento
-    expect(input).toBeInTheDocument()
-
-    // garatir que o botão esteja desabilitado
-    expect(botao).toBeDisabled()
-    
+        mensagemDeErro = screen.queryByRole('alert')
+        expect(mensagemDeErro).toBeNull()
+    })
 })
--- src/componentes/Formulario/Formulario.tsx
+++ src/componentes/Formulario/Formulario.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import styles from './style.module.css'
+import { useAdicionarParticipante } from '../../state/hooks/useAdicionarParticipante';
+import { useMensagemDeErro } from '../../state/hooks/useMensagemDeErro';
 
 export default function Formulario() {
+
+  const [nome, setNome] = useState('');
+
+  const inputRef =useRef<HTMLInputElement>(null)
+
+  const adicionarNaLista = useAdicionarParticipante()
+
+  const mensagemDeErro = useMensagemDeErro()
+
+  const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+    adicionarNaLista(nome)
+    setNome('')
+    inputRef.current?.focus()
+  }
+
   return (
-    <form>
-        <input className={styles.input} type='text' placeholder='Insira os nomes dos participantes'/>
-        <button className={styles.button} disabled={true}>Adicionar</button>
+    <form onSubmit={adicionarParticipante}>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type='text'
+        placeholder='Insira os nomes dos participantes'
+        value={nome}
+        onChange={evento => setNome(evento.target.value)}
+      />
+      <button className={styles.button} disabled={!nome}>Adicionar</button>
+      {mensagemDeErro && <p role='alert'>{mensagemDeErro}</p>}
     </form>
   )
 }
