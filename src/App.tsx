431229f4648be58339add36559c0d8218d6aaf0b
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2023-10-24 09:16:21 -0300

    Refatorando

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import Formulario from './componentes/Formulario/Formulario';
-
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <BrowserRouter>
       <RecoilRoot>
         <Routes>
-          <Route path='/' element={<Formulario/>}>
-          </Route>
+          <Route path='/' element={Formulario}/>
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
